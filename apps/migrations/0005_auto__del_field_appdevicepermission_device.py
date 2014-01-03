2bb6e2fce0d00bc5c7302f16cfd20b1d270984db
Ubuntu <ubuntu@ip-172-31-13-154.us-west-2.compute.internal>
2014-01-03 15:55:40 +0000

    Start splitting settings files

---
+++ apps/migrations/0005_auto__del_field_appdevicepermission_device.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'AppDevicePermission.device'
+        db.delete_column(u'apps_appdevicepermission', 'device_id')
+
+
+    def backwards(self, orm):
+        # Adding field 'AppDevicePermission.device'
+        db.add_column(u'apps_appdevicepermission', 'device',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['devices.Device']),
+                      keep_default=False)
+
+
+    models = {
+        'apps.app': {
+            'Meta': {'object_name': 'App'},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'user_app_creator'", 'null': 'True', 'to': "orm['cb_account.CBAuth']"}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'exe': ('django.db.models.fields.URLField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'modifier': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'user_app_modifier'", 'null': 'True', 'to': "orm['cb_account.CBAuth']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'provider': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '255'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'apps.appdevicepermission': {
+            'Meta': {'object_name': 'AppDevicePermission'},
+            'app_install': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['apps.AppInstall']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'apps.appinstall': {
+            'Meta': {'object_name': 'AppInstall'},
+            'app': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['apps.App']"}),
+            'bridge': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bridges.Bridge']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'bridges.bridge': {
+            'Meta': {'object_name': 'Bridge', '_ormbases': ['cb_account.CBAuth']},
+            u'cbauth_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cb_account.CBAuth']", 'unique': 'True', 'primary_key': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'user_bridge_creator'", 'null': 'True', 'to': "orm['cb_account.CBAuth']"}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'modifier': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'user_bridge_modifier'", 'null': 'True', 'to': "orm['cb_account.CBAuth']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'plaintext_password': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'cb_account.cbauth': {
+            'Meta': {'object_name': 'CBAuth'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['apps']
