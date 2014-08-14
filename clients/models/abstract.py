590c79ab7545d60c23f9ea0a2ab792ff1adbd249
Mark Claydon <developer@glia.co>
2014-08-14 23:59:05 +0100

    Sketching cbid and key auth

--- clients/models/abstract.py
+++ clients/models/abstract.py
@@ -2,10 +2,8 @@
 from django.db import models
 from django.utils.crypto import get_random_string, salted_hmac
 from django.utils.translation import ugettext_lazy as _
-from django.utils import timezone
 from django.contrib.auth.hashers import (
     check_password, make_password, is_password_usable)
-from django.utils.encoding import python_2_unicode_compatible
 
 from polymorphic import PolymorphicModel, PolymorphicManager
 
@@ -43,59 +41,15 @@ class PolymorphicBaseUserManager(PolymorphicManager):
         return self.get(**{self.model.USERNAME_FIELD: username})
 
 
-# Copied from django.contrib.auth.models, modified to be polymorphic
-@python_2_unicode_compatible
-class PolymorphicAbstractBaseUser(PolymorphicModel):
+class AuthKeyMixin(PolymorphicModel):
 
-    last_login = models.DateTimeField(_('last login'), default=timezone.now)
-
-    is_active = True
-
-    REQUIRED_FIELDS = []
-
-    class Meta:
-        abstract = True
-
-    def get_username(self):
-        "Return the identifying username for this User"
-        return getattr(self, self.USERNAME_FIELD)
-
-    def __str__(self):
-        return self.get_username()
-
-    def natural_key(self):
-        return (self.get_username(),)
-
-    def is_anonymous(self):
-        """
-        Always returns False. This is a way of comparing User objects to
-        anonymous users.
-        """
-        return False
-
-    def is_authenticated(self):
-        """
-        Always return True. This is a way to tell if the user has been
-        authenticated in templates.
-        """
-        return True
-
-    def get_full_name(self):
-        raise NotImplementedError('subclasses of AbstractBaseUser must provide a get_full_name() method')
-
-    def get_short_name(self):
-        raise NotImplementedError('subclasses of AbstractBaseUser must provide a get_short_name() method.')
-
-
-class AuthPasswordMixin(PolymorphicModel):
-
-    password = models.CharField(_('password'), max_length=128)
+    key = models.CharField(_('password'), max_length=128)
 
     class Meta:
         abstract = True
 
     def set_password(self, raw_password):
-        self.password = make_password(raw_password)
+        self.key = make_password(raw_password)
 
     def check_password(self, raw_password):
         """
@@ -104,20 +58,20 @@ class AuthPasswordMixin(PolymorphicModel):
         """
         def setter(raw_password):
             self.set_password(raw_password)
-            self.save(update_fields=["password"])
-        return check_password(raw_password, self.password, setter)
+            self.save(update_fields=["key"])
+        return check_password(raw_password, self.key, setter)
 
     def set_unusable_password(self):
         # Sets a value that will never be a valid hash
-        self.password = make_password(None)
+        self.key = make_password(None)
 
     def has_usable_password(self):
-        return is_password_usable(self.password)
+        return is_password_usable(self.key)
 
     def get_session_auth_hash(self):
         """
         Returns an HMAC of the password field.
         """
         key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash"
-        return salted_hmac(key_salt, self.password).hexdigest()
+        return salted_hmac(key_salt, self.key).hexdigest()
 
