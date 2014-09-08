fb657d0ad774cd113f4c9887001b0e437fc2073b
Ubuntu <ubuntu@ip-172-31-20-161.eu-west-1.compute.internal>
2014-09-08 19:40:53 +0000

    Added directories

---
+++ portal/static/js/cb/clients/controls/models.js
@@ -0,0 +1,55 @@
+
+CBApp.ClientControl = Backbone.Deferred.Model.extend({
+
+    idAttribute: 'id',
+
+    backend: 'clientControl',
+
+    relations: [
+        {   
+            type: Backbone.HasOne,
+            key: 'user',
+            keySource: 'user',
+            keyDestination: 'user',
+            relatedModel: 'CBApp.User',
+            collectionType: 'CBApp.UserCollection',
+            createModels: true,
+            includeInJSON: 'resource_uri',
+            initializeCollection: 'userCollection',
+        },
+        {   
+            type: Backbone.HasOne,
+            key: 'client',
+            keySource: 'client',
+            keyDestination: 'client',
+            relatedModel: 'CBApp.Client',
+            collectionType: 'CBApp.ClientCollection',
+            createModels: true,
+            includeInJSON: 'resource_uri',
+            initializeCollection: 'clientCollection',
+            reverseRelation: {
+                type: Backbone.HasMany,
+                key: 'clients',
+                collectionType: 'CBApp.ClientCollection',
+                includeInJSON: false,
+                initializeCollection: 'clientCollection',
+            }   
+        },
+    ]
+}, { modelType: "clientControl" });
+
+CBApp.ClientControlCollection = QueryEngine.QueryCollection.extend({
+
+    model: CBApp.ClientControl,
+    backend: 'clientControl',
+
+    initialize: function() {
+        this.bindBackend();
+        CBApp.ClientControlCollection.__super__.initialize.apply(this, arguments);
+    },
+    
+    parse : function(response){
+        return response.objects;
+    }
+});
+
